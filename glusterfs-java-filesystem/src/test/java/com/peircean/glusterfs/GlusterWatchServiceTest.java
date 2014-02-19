d13793aa41eec81eef102e9c756bffd347d3d7d1
Louis Zuckerman <me@louiszuckerman.com>
2014-02-18 23:44:52 -0500

    watchservice tests

--- glusterfs-java-filesystem/src/test/java/com/peircean/glusterfs/GlusterWatchServiceTest.java
+++ glusterfs-java-filesystem/src/test/java/com/peircean/glusterfs/GlusterWatchServiceTest.java
@@ -8,12 +8,17 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.IOException;
 import java.nio.file.ClosedWatchServiceException;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
-import static org.powermock.api.mockito.PowerMockito.*;
+import static org.junit.Assert.assertFalse;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 //import static org.mockito.Mockito.*;
 
@@ -21,21 +26,125 @@ import static org.powermock.api.mockito.PowerMockito.*;
 @PrepareForTest({GlusterWatchService.class, Thread.class})
 public class GlusterWatchServiceTest {
 
-    GlusterWatchService watchService = spy(new GlusterWatchService());
+    GlusterWatchService watchService = PowerMockito.spy(new GlusterWatchService());
+
+    @Test(expected = ClosedWatchServiceException.class)
+    public void testRegisterPath_whenNotRunning() {
+        watchService.setRunning(false);
+        GlusterPath mockPath = mock(GlusterPath.class);
+        watchService.registerPath(mockPath);
+    }
 
     @Test
     public void testRegisterPath() {
+        watchService.setRunning(true);
+        GlusterPath mockPath = mock(GlusterPath.class);
+        WatchEvent.Kind mockKind = mock(WatchEvent.Kind.class);
+        watchService.registerPath(mockPath, mockKind);
+        GlusterWatchKey event = watchService.getPaths().iterator().next();
+        assertEquals(mockPath, event.getPath());
+        assertEquals(mockKind, event.getKinds()[0]);
+    }
+
+    @Test
+    public void testRegisterPath_whenPathExists() {
+        watchService.setRunning(true);
+        GlusterPath mockPath = mock(GlusterPath.class);
+        GlusterWatchKey keyFix = new GlusterWatchKey(mockPath, new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_MODIFY});
+        watchService.getPaths().add(keyFix);
+        watchService.registerPath(mockPath, StandardWatchEventKinds.ENTRY_CREATE);
+        GlusterWatchKey event = watchService.getPaths().iterator().next();
+        assertEquals(mockPath, event.getPath());
+        assertEquals(StandardWatchEventKinds.ENTRY_CREATE, event.getKinds()[0]);
 
     }
 
     @Test
-    public void testClose() {
-
+    public void testClose_whenNotRunning() throws IOException {
+        watchService.setRunning(false);
+        GlusterWatchKey mockKey = mock(GlusterWatchKey.class);
+        watchService.getPaths().add(mockKey);
+        watchService.close();
+        Mockito.verify(mockKey, Mockito.never()).cancel();
     }
 
     @Test
-    public void testPoll() {
+    public void testClose() throws IOException {
+        watchService.setRunning(true);
+        GlusterWatchKey mockKey = mock(GlusterWatchKey.class);
+        watchService.getPaths().add(mockKey);
+        watchService.close();
+        Mockito.verify(mockKey).cancel();
+        assertFalse(watchService.isRunning());
+    }
 
+    @Test
+    public void testPopPending_whenNonePending() {
+        WatchKey key = watchService.popPending();
+        assertEquals(null, key);
+        assertEquals(0, watchService.getPendingPaths().size());
+    }
+
+    @Test
+    public void testPopPending() {
+        GlusterPath mockPath = mock(GlusterPath.class);
+        GlusterWatchKey keyFix = new GlusterWatchKey(mockPath, new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_CREATE});
+        watchService.getPendingPaths().add(keyFix);
+        WatchKey key = watchService.popPending();
+        assertEquals(keyFix, key);
+        assertEquals(0, watchService.getPendingPaths().size());
+    }
+
+    @Test(expected = ClosedWatchServiceException.class)
+    public void testPoll_whenNotRunning() {
+        watchService.setRunning(false);
+        watchService.poll();
+    }
+
+    @Test
+    public void testPoll_whenPending() {
+        watchService.setRunning(true);
+        GlusterWatchKey mockKey = mock(GlusterWatchKey.class);
+        watchService.getPendingPaths().add(mockKey);
+        WatchKey key = watchService.poll();
+        assertEquals(mockKey, key);
+    }
+
+    @Test
+    public void testPoll_whenReadyAndEvent() {
+        watchService.setRunning(true);
+        GlusterWatchKey mockKey = mock(GlusterWatchKey.class);
+        watchService.getPaths().add(mockKey);
+        doReturn(null).doReturn(mockKey).when(watchService).popPending();
+        doReturn(true).when(mockKey).isValid();
+        doReturn(true).when(mockKey).isReady();
+        doReturn(true).when(mockKey).update();
+
+        WatchKey key = watchService.poll();
+
+        assertEquals(mockKey, key);
+
+        Mockito.verify(mockKey).isValid();
+        Mockito.verify(mockKey).isReady();
+        Mockito.verify(mockKey).update();
+    }
+
+    @Test
+    public void testPoll_whenReadyAndNoEvent() {
+        watchService.setRunning(true);
+        GlusterWatchKey mockKey = mock(GlusterWatchKey.class);
+        watchService.getPaths().add(mockKey);
+        doReturn(true).when(mockKey).isValid();
+        doReturn(true).when(mockKey).isReady();
+        doReturn(false).when(mockKey).update();
+
+        WatchKey key = watchService.poll();
+
+        assertEquals(null, key);
+
+        Mockito.verify(mockKey).isValid();
+        Mockito.verify(mockKey).isReady();
+        Mockito.verify(mockKey).update();
     }
 
     @Test(expected = ClosedWatchServiceException.class)
