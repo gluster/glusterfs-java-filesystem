c527505384dc5fa4727f4e54bcda3a840cbf006f
Louis Zuckerman <me@louiszuckerman.com>
2014-02-26 00:13:56 -0500

    vagrant [ci skip]

---
+++ vagrant-provisioner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+add-apt-repository -y ppa:semiosis/ubuntu-glusterfs-3.4
+apt-get update
+# apt-get -y dist-upgrade
+apt-get -y install glusterfs-server
+
+sed -i 's/\(end-volume\)/    option rpc-auth-allow-insecure on\n\1/' /etc/glusterfs/glusterd.vol
+service glusterfs-server restart
+echo 127.0.2.1 gluster >> /etc/hosts
+gluster volume create foo gluster:/var/tmp/foo force
+gluster volume set foo server.allow-insecure on
+gluster volume start foo
+mkdir /mnt/foo
+mount -t glusterfs localhost:foo /mnt/foo
