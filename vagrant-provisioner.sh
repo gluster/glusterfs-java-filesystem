7c5137cbb8ac0083fed809a7aabcf1d6b81a2e83
Louis Zuckerman <me@louiszuckerman.com>
2016-10-02 12:44:48 -0400

    - Update vagrant box to xenial - Update glusterfs version installed in vagrant box to 3.8 - Update lombok version

--- vagrant-provisioner.sh
+++ vagrant-provisioner.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-add-apt-repository -y ppa:gluster/glusterfs-3.4
+add-apt-repository -y ppa:gluster/glusterfs-3.8
 apt-get update
 # apt-get -y dist-upgrade
 apt-get -y install glusterfs-server
+sleep 2
 
 sed -i 's/\(end-volume\)/    option rpc-auth-allow-insecure on\n\1/' /etc/glusterfs/glusterd.vol
 service glusterfs-server restart
+sleep 2
+
 gluster volume create foo ${1}:/var/tmp/foo force
 gluster volume set foo server.allow-insecure on
 gluster volume start foo
-mkdir /mnt/foo
-mount -t glusterfs localhost:foo /mnt/foo
-chmod ugo+w /mnt/foo
+sleep 2
 
+mkdir -v /mnt/foo
+mount -t glusterfs localhost:foo /mnt/foo && echo Mounted glusterfs volume at /mnt/foo.
+chmod -v ugo+w /mnt/foo
+
+echo Provision complete.
