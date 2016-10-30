c166676162bd78031d402e188252ecef60c11dfd
Louis Zuckerman <me@louiszuckerman.com>
2016-10-30 14:52:21 -0400

    update travis.ci

--- catlogs.sh
+++ catlogs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-for i in `find . -path '**/surefire-reports/**'`; do
+echo
+echo '*** LIBGFAPI Client Log:'
+cat /tmp/glfsjni.log
+echo '***'
+
+for i in `find . -path '**/surefire-reports/**.txt'`; do
     echo
-    echo
-    echo $i
+    echo "*** MVN LOG: $i"
     cat $i
+    echo '***'
 done
